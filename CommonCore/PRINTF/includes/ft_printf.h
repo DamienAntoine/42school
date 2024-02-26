81daab346e65ca9fd3a42ab4c29a6d8d452bc79f
Damien Antoine <dantoine@c2r5s2.42luxembourg.lu>
2024-02-26 17:19:26 +0100

    progressed on printf

--- CommonCore/PRINTF/includes/ft_printf.h
+++ CommonCore/PRINTF/includes/ft_printf.h
@@ -1,5 +1,7 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+#include <stdarg.h>
+#include <unistd.h>
 # include "libft.h"
 
 #endif
--- CommonCore/PRINTF/srcs/ft_printf.c
+++ CommonCore/PRINTF/srcs/ft_printf.c
@@ -1,10 +1,10 @@
-#include <stdarg.h>
-#include <unistd.h>
+#include "libft.h"
+#include "ft_printf.h"
 
 int	ft_putchar(char c) //%c Prints a single character.
 {
 	write(1, &c, 1);
-	return (0);
+	return (1);
 }
 
 int	ft_putstr(char *s) //%s Prints a string.
@@ -17,19 +17,19 @@ int	ft_putstr(char *s) //%s Prints a string.
 		write(1, s[i], 1);
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
-void	ft_putnbr(int n)
+int	ft_putnbr(int n)
 {
 	if (n == -2147483648)
 	{
-		ft_putstr("-2147483648");
+		ft_putstr('-2147483648');
 		return ;
 	}
 	if (n == -987441)
 	{
-		ft_putstr("-987441");
+		ft_putstr('-987441');
 		return ;
 	}
 	else
@@ -43,37 +43,49 @@ void	ft_putnbr(int n)
 		{
 			ft_putnbr(n / 10);
 		}
-		ft_putchar_fd((n % 10) + '0');
+		ft_putchar((n % 10) + '0');
 	}
+	return (1);
 }
 
-int	ft_atoi(const char *str)
+int	checkformat(va_list args, const char format, ...)
 {
-	int	sign;
-	int	result;
-	int	i;
+	size_t	input_counter;
 
-	sign = 1;
-	result = 0;
-	i = 0;
-	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
+	input_counter = 0;
+	if (format == '%c')
 	{
-		i++;
+		input_counter += ft_putchar(va_arg(args, int));
 	}
-	if (str[i] == '-' || str[i] == '+')
+	if (format == '%s')
 	{
-		if (str[i] == '-')
-		{
-			sign *= -1;
-		}
-		i++;
+
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	if (format == '%p')
 	{
-		result = result * 10 + (str[i] - '0');
-		i++;
+
 	}
-	return (sign * result);
+	if (format == '%d')
+	{
+		
+	}
+	if (format == '%i')
+	{
+
+	}
+	if (format == '%u')
+	{
+		
+	}
+	if (format == '%x' || format == '%X')
+	{
+
+	}
+	if (format == '%%')
+	{
+		
+	}
+	return (input_counter);
 }
 
 int	ft_printf(const char *format, ...)
