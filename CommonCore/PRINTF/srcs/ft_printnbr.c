b993d240e2a78b517ec22540296098b57c615d48
DamienAntoine <158172677+DamienAntoine@users.noreply.github.com>
2024-02-27 23:36:46 +0100

    Update ft_printnbr.c

--- CommonCore/PRINTF/srcs/ft_printnbr.c
+++ CommonCore/PRINTF/srcs/ft_printnbr.c
@@ -30,3 +30,22 @@ int	ft_printnbr(int n)
 	}
 	return (1);
 }
+
+int	ft_print_digit(long n, int base)
+{
+	int count;
+	char *symbols;
+	symbols = "0123456789abcdef";
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		return ft_print_digit(-n, base) + 1;
+	}
+	else if (n < base)
+		return ft_print_char(symbols[n]);
+	else
+	{
+		count = print_digit(n / base, base);
+		return count + print_digit(n % base, base);
+	}
+}
