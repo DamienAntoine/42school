31b23945f0bd4607aee3500e0192b8765077d03f
gru <damienantoine57@gmail.com>
2024-03-03 22:25:23 +0100

    changed

--- CommonCore/GETNEXTLINE/get_next_line.c
+++ CommonCore/GETNEXTLINE/get_next_line.c
@@ -5,6 +5,9 @@ char	*get_next_line(int fd)
 	static int	buffer;
 	size_t bytesread;
 
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return (NULL);
+	while (fd
 	bytesread = read(fd, buffer, BUFFER_SIZE - 1);
 	if (bytesread == -1)
 		error;
