e1c02a489f82226a32ea4e3fcc96010f6a989d92
Damien Antoine <dantoine@c2r5s2.42luxembourg.lu>
2024-04-17 15:31:37 +0200

    ffffffffffffffffff

--- CommonCore/MINITALK/srcs/client_bonus.c
+++ CommonCore/MINITALK/srcs/client_bonus.c
@@ -1,6 +1,6 @@
 #include "../headers/minitalk.h"
 
-void    signal_sender(int pid, char c, int client_pid)
+void    signal_sender(int pid, char c)
 {
         int     i;
 
@@ -16,12 +16,6 @@ void    signal_sender(int pid, char c, int client_pid)
                 i++;
                 usleep(1000);
         }
-	while (1)
-	{
-		signal(SIGUSR1, signal_confirm);
-		signal(SIGUSR2, signal_confirm);
-		pause();
-	}
 }
 
 void    signal_confirm(int c)
@@ -51,32 +45,11 @@ void    signal_confirm(int c)
         }
 }
 
-void	send_clientpid(int client_pid)
-{
-	int     i;
-
-        i = 0;
-        while (i < 8)
-        {
-                if (c & (1 << i))
-                {
-                        kill(pid, SIGUSR1);
-                }
-                else
-                        kill(pid, SIGUSR2);
-                i++;
-                usleep(1000);
-        }
-}
-
 int	main(int argc, char **argv)
 {
 	int	pid;
 	int	i;
-	int	client_pid;
 
-        client_pid = getpid();
-	send_clientpid(client_pid);
 	i = 0;
 	if (argc == 3)
 	{
