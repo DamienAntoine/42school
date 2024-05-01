233dffa3cc7b2dd4b6626b90cb6ea4f58c398b26
gru <damienantoine57@gmail.com>
2024-05-01 21:51:27 +0200

    pushswap

--- CommonCore/PUSH_SWAP/srcs/main.c
+++ CommonCore/PUSH_SWAP/srcs/main.c
@@ -1,12 +1,67 @@
+void    populate_struct(int value)
+{
+
+}
+
+void    *str_to_int(char *str)
+{
+    int i;
+
+    i = 0;
+    
+    populate_struct(ft_atoi()
+}
+
+void    handle_arrays(char **str)
+{
+    int i;
+    int res;
+    char    *split_str;
+
+    i = 0;
+    while (str[i])
+    {
+        split_str = ft_strdup(str[i]);
+        res = ft_atoi(split_str);
+        populate_struct(res);
+        free(split_str);
+        i++;
+    }
+}
+
 int main(int argc, char **argv)
 {
-    void    *astack;
-    void    *bstack;
+    int    *astack;
+    int    *bstack;
+    int     i;
+    int     j;
 
     if (argc < 2)
 	    return (0);
     if (!check_input(argv))
 	    exit_error(NULL, NULL);
     bstack = NULL;
-    astack =
+    if (argc > 1)
+    {
+        i = 0;
+        j = 0;
+        while (argv[i])
+        {
+            while (argv[i][j])
+            {
+                if (argv[i][j] == ' ')
+                {
+                    handle_arrays(ft_split(argv[i], ' '));
+
+                }
+                else
+                {
+                    populate_struct(ft_atoi(argv[i]));
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+    
 }
--- CommonCore/PUSH_SWAP/srcs/push.c
+++ CommonCore/PUSH_SWAP/srcs/push.c
@@ -3,12 +3,13 @@ static void	push(t_stack **src, t_stack **dest)
 
 }
 
+//pa (push a): Take the first element at the top of b and put it at the top of a. Do nothing if b is empty
 void	pa(t_stack **astack, t_stack **bstack)
 {
 	push(bstack, astack);
 	ft_putstr("pa\n");
 }
-
+//pb (push b): Take the first element at the top of a and put it at the top of b. Do nothing if a is empty
 void    pb(t_stack **astack, t_stack **bstack)
 {
         push(astack, bstack);
