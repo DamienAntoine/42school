079ee8a4f66340b47ebae3e917f4d356eff2ea13
Damien Antoine <dantoine@c1r9s2.42luxembourg.lu>
2024-07-05 15:50:49 +0200

    Fractol

--- CommonCore/FRACTOL/srcs/utils.c
+++ CommonCore/FRACTOL/srcs/utils.c
@@ -25,6 +25,7 @@ int	exit_program(t_fractal_data *data)
 {
 	mlx_destroy_image(data->mlx, data->img);
 	mlx_destroy_window(data->mlx, data->win);
+    mlx_destroy_display(data->mlx);
 	free(data->mlx);
 	free(data);
 	exit(1);
