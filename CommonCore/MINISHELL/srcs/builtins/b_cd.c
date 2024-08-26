90720dc99c030f511474870fd48b47977f7d090c
Sang Ha Hwang <putcharmaster@gmail.com>
2024-08-26 15:32:30 +0200

    sam : b_cd added

--- CommonCore/MINISHELL/srcs/builtins/b_cd.c
+++ CommonCore/MINISHELL/srcs/builtins/b_cd.c
@@ -4,5 +4,15 @@
 //also with absolute path (from home directory)
 void    cd(t_command *current)
 {
-    
+    if (current->args[1])
+    {
+        printf("Too many args for cd command");
+        return ;
+    }
+    if (!current->args[0])
+        //move to home
+    else if (!ft_strcmp(current->args[0], ".."))
+        // move one up
+    else (current->args[0])
+        // move to this path(current->args[i])
 }
