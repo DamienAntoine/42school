5acd1d962ff89aba106baee3c0eb4757dc01082c
gru <damienantoine57@gmail.com>
2024-08-27 15:19:10 +0200

    added syntax checks

--- CommonCore/MINISHELL/headers/minishell.h
+++ CommonCore/MINISHELL/headers/minishell.h
@@ -23,11 +23,11 @@ typedef struct s_env
 //and somehow separate the command from the flags and store the flags in another variable
 typedef struct s_command
 {
-	char				*cmds;				//command (ls, env, cd, ...)
-	char				**args;				//arguments / flags (-l, "file.txt", ...)
-	t_env				*env;	//!!!!!!!!!!ADDED
-	t_redirection		*redirections;		//use this instead of the three variables above
-	struct s_command	*next;				//pointer to next command if pipe (|)
+	char					*cmds;				//command (ls, env, cd, ...)
+	char					**args;				//arguments / flags (-l, "file.txt", ...)
+	t_env					*env;	//!!!!!!!!!!ADDED
+	struct s_redirection	*redirections;		//use this instead of the three variables above
+	struct s_command		*next;				//pointer to next command if pipe (|)
 }	t_command;
 
 typedef struct s_redirection				//this struct is to be able to use multiple redirections in one command
@@ -35,7 +35,7 @@ typedef struct s_redirection				//this struct is to be able to use multiple redi
 	char					*file;			// file for redirection
 	int						type;			// 0 for input ("<"), 1 for output (">"), 2 for append (">>")
 	struct	s_redirection	*next;			// pointer to the next redirection
-} t_redirection;
+}	t_redirection;
 
 typedef struct s_token_list
 {
