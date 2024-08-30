045ff40c3b033a8f078adbd1baf53c113376bf17
gru <damienantoine57@gmail.com>
2024-08-30 12:21:59 +0200

    better minishell_init

--- CommonCore/MINISHELL/srcs/main.c
+++ CommonCore/MINISHELL/srcs/main.c
@@ -143,37 +143,37 @@ t_data	*init_minishell(char **env)
 	data->commands = malloc(sizeof(t_command));
 	data->toklist = malloc(sizeof(t_token_list));
 	data->env = malloc(sizeof(t_env));
-	init_env(env, data->env);
-    init_commands(data);
-    init_toklist(data);
-	if (!data || !data->commands || !data->toklist)
+	if (!data->commands || !data->toklist || !data->env)
+	{
+		free(data->commands);
+		free(data->toklist);
+		free(data->env);
+		free(data);
 		return (NULL);
+	}
+	init_env(env, data->env);
+	init_commands(data);
+	data->toklist->tokens = NULL;
 	return (data);
 }
 
 void    init_commands(t_data *data)
 {
-    data->commands->args = NULL;
-    data->commands->cmds = NULL;
-    data->commands->env = NULL;
-    data->commands->next = NULL;
-    data->commands->redirections = NULL;
-}
-
-void    init_toklist(t_data *data)
-{
-    data->toklist->tokens = NULL;
-    //data->toklist->token_count = 0;
+	data->commands->args = NULL;
+	data->commands->cmds = NULL;
+	data->commands->env = NULL;
+	data->commands->next = NULL;
+	data->commands->redirections = NULL;
 }
 
 int     ft_token_counter(t_token_list *toklist)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (toklist->tokens[i])
-        i++;
-    return (i);
+	i = 0;
+	while (toklist->tokens[i])
+		i++;
+	return (i);
 }
 
 void printcommands(t_command *commands)
