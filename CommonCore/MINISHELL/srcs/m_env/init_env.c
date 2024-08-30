f6e65dcbf28f9718ec85aed063bfa7be6e21bf08
gru <damienantoine57@gmail.com>
2024-08-30 12:47:21 +0200

    Tidying a bit

--- CommonCore/MINISHELL/srcs/m_env/init_env.c
+++ CommonCore/MINISHELL/srcs/m_env/init_env.c
@@ -1,8 +1,8 @@
 #include "../../headers/minishell.h"
 
-//init (when starting minishell)
-//to print with the env cmd, just go through every node and print "NAME"+"="+"value"+newline
-void init_env(char **env, t_env **cur_env)
+// init (when starting minishell)
+// to print with the env cmd, just go through every node and print "NAME"+"="+"value"+newline
+void	init_env(char **env, t_env **cur_env)
 {
 	int		i;
 	char	**temp;
@@ -10,14 +10,14 @@ void init_env(char **env, t_env **cur_env)
 	t_env	*last_node;
 
 	i = 0;
-	*cur_env = NULL; //init linked list
+	*cur_env = NULL; // init linked list
 	while (env[i])
 	{
 		temp = ft_split(env[i], '=');
-		new_node = (t_env *)malloc(sizeof(t_env)); //new node
+		new_node = (t_env *)malloc(sizeof(t_env)); // new node
 		if (!new_node)
 			return ;
-		new_node->type = strdup(temp[0]); // copy var name
+		new_node->type = strdup(temp[0]);  // copy var name
 		new_node->value = strdup(temp[1]); // copy value
 		new_node->next = NULL;
 		// if first node (first loop)
@@ -38,46 +38,58 @@ void init_env(char **env, t_env **cur_env)
 	}
 }
 
-//use this to update an env variable (after cd for example to update PWD), instead of running init again which would reset the whole env
-void update_env_variable(t_env *env_list, char *name, char *value)
+// use this to update an env variable (after cd for example to update PWD), instead of running init again which would reset the whole env
+void	update_env_variable(t_env *env_list, char *name, char *value)
 {
-	t_env *current;
+	t_env	*current;
 
 	current = env_list;
 	while (current)
 	{
 		if (strcmp(current->type, name) == 0)
 		{
-			free(current->value); // free old value
+			free(current->value);           // free old value
 			current->value = strdup(value); // set new value
-			return;
+			return ;
 		}
 		current = current->next;
 	}
 }
 
-//use to unset a variable (like raja showed with "unset pwd")
-void unset_env_variable(t_env **env_list, char *name)
+// use to unset a variable (like raja showed with "unset pwd")
+void	unset_env_variable(t_env **env_list, char *name)
 {
-	t_env *current;
-	t_env *previous;
+	t_env	*current;
+	t_env	*previous;
 
 	current = *env_list;
 	previous = NULL;
 	while (current)
 	{
-		if (strcmp(current->type, name) == 0)//goes through every nodes until it finds the target
+		if (strcmp(current->type, name) == 0)
+			// goes through every nodes until it finds the target
 		{
-			if (!previous)//if there is no node before Current, means its the first node of the list
+			if (!previous) // if there is no node before Current, means its the first node of the list
 				*env_list = current->next;
 			else
 				previous->next = current->next; // theres a node before current,
-			free(current->type); // free name
-			free(current->value); // free value
-			free(current); // free node
-			return;
+			free(current->type);                // free name
+			free(current->value);               // free value
+			free(current);                      // free node
+			return ;
 		}
 		previous = current;
 		current = current->next;
 	}
+}
+
+char	*find_env_value(t_env *env, const char *name)
+{
+	while (env != NULL)
+	{
+		if (!ft_strcmp(env->type, name))
+			return (env->value);
+		env = env->next;
+	}
+	return (NULL); // Return NULL if the variable is not found
 }
---
+++ CommonCore/MINISHELL/srcs/m_init/init.c
@@ -0,0 +1,34 @@
+#include "../headers/minishell.h"
+
+t_data	*init_minishell(char **env)
+{
+	t_data *data;
+
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (NULL);
+	data->commands = malloc(sizeof(t_command));
+	data->toklist = malloc(sizeof(t_token_list));
+	data->env = malloc(sizeof(t_env));
+	if (!data->commands || !data->toklist || !data->env)
+	{
+		free(data->commands);
+		free(data->toklist);
+		free(data->env);
+		free(data);
+		return (NULL);
+	}
+	init_env(env, &data->env);
+	init_commands(data);
+	data->toklist->tokens = NULL;
+	return (data);
+}
+
+void    init_commands(t_data *data)
+{
+	data->commands->args = NULL;
+	data->commands->cmds = NULL;
+	data->commands->env = NULL;
+	data->commands->next = NULL;
+	data->commands->redirections = NULL;
+}
