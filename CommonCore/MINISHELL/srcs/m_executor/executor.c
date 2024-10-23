4120ab3e0fb79ec1ef77d5556bb514c8a099b10b
Sang Ha Hwang <putcharmaster@gmail.com>
2024-10-23 19:24:00 +0200

    splitted more

--- CommonCore/MINISHELL/srcs/m_executor/executor.c
+++ CommonCore/MINISHELL/srcs/m_executor/executor.c
@@ -1,79 +1,106 @@
 #include "../../headers/minishell.h"
 
-// int	check_redirection_before_fork(t_data *data)
-// {
-// 	t_command		*cmdtable;
-// 	int				fd;
-// 	t_redirection	*redir;
 
-// 	cmdtable = data->commands;
-// 	while (cmdtable)
-// 	{
-// 		redir = cmdtable->redirects;
-// 		while (redir)
-// 		{
-// 			fd = open_redirection(redir);
-// 			if (fd == -1)
-// 			{
-// 				perror(redir->file);
-// 				return (-1);
-// 			}
-// 			if (redir->type != 3)
-// 				close(fd);
-// 			redir = redir->next;
-// 		}
-// 		cmdtable = cmdtable->next;
-// 	}
-// 	return (0);
-// }
 
-int	send_command(t_data *data)
+int	handle_null_data(t_data *data)
 {
-	char		**envp;
-	t_command	*cmdtable;
-	int			arg_count;
-	char		**full_args;
-	int			exit_code;
-	char		*command_path;
-
-	envp = env_list_to_array(data->env);
-	cmdtable = data->commands;
-	full_args = prepare_full_args(cmdtable, &arg_count);
-	exit_code = 0;
 	if (data == NULL)
 	{
 		fprintf(stderr, "Error: Data pointer is NULL.\n");
 		return (1);
 	}
-	data->error_occurred = 0;
-	if (!cmdtable || !cmdtable->cmds)
+	return (0);
+}
+
+int	handle_null_commands(t_data *data, char **envp)
+{
+	if (!data->commands || !data->commands->cmds)
 	{
 		free_split(envp);
 		data->state.last_exit_status = 0;
 		return (0);
 	}
-	if (is_builtin(cmdtable->cmds))
-		exit_code = execute_builtin_command(cmdtable, data);
-	else
+	return (-1);
+}
+
+int	handle_builtin_command(t_data *data, int *exit_code)
+{
+	if (is_builtin(data->commands->cmds))
 	{
-		command_path = get_command_path(cmdtable->cmds);
-		if (!command_path)
-		{
-			exit_code = handle_exec_error(data, cmdtable->cmds);
-			free_split(envp);
-			free(full_args);
-			set_exit_status(exit_code, data);
-			return (exit_code);
-		}
-		exit_code = execute_external_command(cmdtable, full_args, envp, data);
-		if (exit_code == 126 || exit_code == 127)
-			exit_code = handle_exec_error(data, command_path);
-		free(command_path);
+		*exit_code = execute_builtin_command(data->commands, data);
+		return (1);
 	}
+	return (0);
+}
+
+int	prepare_command(t_data *data, char ***envp, char ***full_args)
+{
+	int	arg_count;
+	*envp = env_list_to_array(data->env);
+	*full_args = prepare_full_args(data->commands, &arg_count);
+
+	if (handle_null_data(data))
+		return (1);
+	data->error_occurred = 0;
+	if (handle_null_commands(data, *envp) == 0)
+		return (0);
+	return (-1);
+}
+
+int	handle_builtin_cmd(t_data *data, char **envp, char **full_args)
+{
+	int exit_code;
+
+	if (is_builtin(data->commands->cmds))
+	{
+		exit_code = execute_builtin_command(data->commands, data);
+		free_split(envp);
+		free(full_args);
+		set_exit_status(exit_code, data);
+		return (exit_code);
+	}
+	return (-1);
+}
+
+int	execute_cmd(t_data *data, char **envp, char **full_args)
+{
+	int		exit_code;
+	char	*command_path;
+
+	exit_code = handle_builtin_cmd(data, envp, full_args);
+	if (exit_code != -1)
+		return (exit_code);
+	command_path = get_command_path(data->commands->cmds);
+	if (!command_path)
+	{
+		exit_code = handle_exec_error(data, data->commands->cmds);
+		free_split(envp);
+		free(full_args);
+		set_exit_status(exit_code, data);
+		return (exit_code);
+	}
+	exit_code = execute_external_command(data->commands, full_args, envp, data);
+	if (exit_code == 126 || exit_code == 127)
+		exit_code = handle_exec_error(data, command_path);
+	free(command_path);
 	free_split(envp);
 	free(full_args);
 	set_exit_status(exit_code, data);
+
 	return (exit_code);
+}
+
+int send_command(t_data *data)
+{
+	char	**envp;
+	char	**full_args;
+	int		result;
+
+	result = prepare_command(data, &envp, &full_args);
+	if (result != -1)
+		return result;
+
+	return execute_cmd(data, envp, full_args);
 }
 
 int	ft_cmdsize(t_command *lst)
