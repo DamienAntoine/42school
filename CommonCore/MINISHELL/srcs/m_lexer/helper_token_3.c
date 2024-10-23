416ced8b62bd1b94cb1c16a2c6f7819c482168e0
sangha <putcharmaster@gmail.com>
2024-10-23 20:48:02 +0200

    norminette helper

--- CommonCore/MINISHELL/srcs/m_lexer/helper_token_3.c
+++ CommonCore/MINISHELL/srcs/m_lexer/helper_token_3.c
@@ -7,15 +7,19 @@ void	quote_check(char c, int *in_single_quotes, int *in_double_quotes)
 	else if (c == '\"' && !(*in_single_quotes))
 		*in_double_quotes = !(*in_double_quotes);
 }
-void	process_input(const char *input, char *result, size_t len, int *in_single_quotes, int *in_double_quotes)
+void	process_input(const char *input, char *result, size_t len,
+		int *in_single_quotes, int *in_double_quotes)
 {
-	size_t	i = 0;
-	size_t	j = 0;
+	size_t	i;
+	size_t	j;
 
+	i = 0;
+	j = 0;
 	while (i < len)
 	{
 		quote_check(input[i], in_single_quotes, in_double_quotes);
-		if ((input[i] == '\'' && !(*in_double_quotes)) || (input[i] == '\"' && !(*in_single_quotes)))
+		if ((input[i] == '\'' && !(*in_double_quotes)) || (input[i] == '\"'
+				&& !(*in_single_quotes)))
 			i++;
 		else
 			result[j++] = input[i++];
@@ -27,14 +31,15 @@ char	*remove_balanced_quotes(const char *input)
 {
 	char	*result;
 	size_t	len;
-	int		in_single_quotes = 0;
-	int		in_double_quotes = 0;
+	int		in_single_quotes;
+	int		in_double_quotes;
 
+	in_single_quotes = 0;
+	in_double_quotes = 0;
 	len = ft_strlen(input);
 	result = malloc(len + 1);
 	if (!result)
 		return (NULL);
 	process_input(input, result, len, &in_single_quotes, &in_double_quotes);
-
 	return (result);
 }
