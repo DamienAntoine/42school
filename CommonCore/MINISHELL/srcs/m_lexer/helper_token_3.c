3740344924a64616f272fb63bba858e46084762d
Sang Ha Hwang <putcharmaster@gmail.com>
2024-10-23 20:47:33 +0200

    splitttttt

--- CommonCore/MINISHELL/srcs/m_lexer/helper_token_3.c
+++ CommonCore/MINISHELL/srcs/m_lexer/helper_token_3.c
@@ -7,33 +7,34 @@ void	quote_check(char c, int *in_single_quotes, int *in_double_quotes)
 	else if (c == '\"' && !(*in_single_quotes))
 		*in_double_quotes = !(*in_double_quotes);
 }
-
-char	*remove_balanced_quotes(const char *input)
+void	process_input(const char *input, char *result, size_t len, int *in_single_quotes, int *in_double_quotes)
 {
-	char	*result;
-	size_t	len;
-	size_t	i;
-	size_t	j;
-	int		in_single_quotes;
-	int		in_double_quotes;
+	size_t	i = 0;
+	size_t	j = 0;
 
-	i = 0;
-	j = 0;
-	in_single_quotes = 0;
-	in_double_quotes = 0;
-	len = ft_strlen(input);
-	result = malloc(len + 1);
-	if (!result)
-		return (NULL);
 	while (i < len)
 	{
-		quote_check(input[i], &in_single_quotes, &in_double_quotes);
-		if ((input[i] == '\'' && !in_double_quotes) || (input[i] == '\"'
-				&& !in_single_quotes))
+		quote_check(input[i], in_single_quotes, in_double_quotes);
+		if ((input[i] == '\'' && !(*in_double_quotes)) || (input[i] == '\"' && !(*in_single_quotes)))
 			i++;
 		else
 			result[j++] = input[i++];
 	}
 	result[j] = '\0';
+}
+
+char	*remove_balanced_quotes(const char *input)
+{
+	char	*result;
+	size_t	len;
+	int		in_single_quotes = 0;
+	int		in_double_quotes = 0;
+
+	len = ft_strlen(input);
+	result = malloc(len + 1);
+	if (!result)
+		return (NULL);
+	process_input(input, result, len, &in_single_quotes, &in_double_quotes);
+
 	return (result);
 }
