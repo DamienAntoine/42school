9c60b0a33b1ef927da59f0231f268d6667600234
sangha <putcharmaster@gmail.com>
2024-10-23 18:27:42 +0200

    norminette exe_1 exe_2 redirection etc

--- CommonCore/MINISHELL/srcs/utils/util_exe_1.c
+++ CommonCore/MINISHELL/srcs/utils/util_exe_1.c
@@ -24,10 +24,8 @@ int	handle_is_directory(t_data *data, char *exec_target, struct stat path_stat)
 
 int	handle_permission_denied(t_data *data, char *exec_target)
 {
-	if (access(exec_target, F_OK) == 0
-		&& access(exec_target, R_OK) == -1
-		&& access(exec_target, W_OK) == -1
-		&& access(exec_target, X_OK) == -1)
+	if (access(exec_target, F_OK) == 0 && access(exec_target, R_OK) == -1
+		&& access(exec_target, W_OK) == -1 && access(exec_target, X_OK) == -1)
 	{
 		ft_putstr_fd(exec_target, STDERR_FILENO);
 		ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
@@ -44,7 +42,8 @@ int	handle_permission_denied(t_data *data, char *exec_target)
 	return (0);
 }
 
-int	handle_exec_permission(t_data *data, char *exec_target, struct stat path_stat)
+int	handle_exec_permission(t_data *data, char *exec_target,
+		struct stat path_stat)
 {
 	if (S_ISREG(path_stat.st_mode) && access(exec_target, X_OK) == -1)
 	{
@@ -59,25 +58,22 @@ int	handle_exec_permission(t_data *data, char *exec_target, struct stat path_sta
 	return (0);
 }
 
-int	handle_exec_error_if_part(t_data *data, char *exec_target, struct stat path_stat)
+int	handle_exec_error_if_part(t_data *data, char *exec_target,
+		struct stat path_stat)
 {
-	int result;
+	int	result;
 
 	result = handle_is_directory(data, exec_target, path_stat);
 	if (result != 0)
-		return result;
-
+		return (result);
 	result = handle_permission_denied(data, exec_target);
 	if (result != 0)
-		return result;
-
+		return (result);
 	result = handle_exec_permission(data, exec_target, path_stat);
 	if (result != 0)
-		return result;
-
+		return (result);
 	return (0);
 }
-
 
 int	handle_exec_error_else_part(t_data *data, char *exec_target)
 {
--- CommonCore/MINISHELL/srcs/utils/util_exe_2.c
+++ CommonCore/MINISHELL/srcs/utils/util_exe_2.c
@@ -1,6 +1,5 @@
 #include "../../headers/minishell.h"
 
-
 int	handle_exec_error(t_data *data, char *exec_target)
 {
 	struct stat	path_stat;
@@ -8,35 +7,34 @@ int	handle_exec_error(t_data *data, char *exec_target)
 	if (data->error_occurred != 1)
 	{
 		if (stat(exec_target, &path_stat) == 0)
-			return handle_exec_error_if_part(data, exec_target, path_stat);
+			return (handle_exec_error_if_part(data, exec_target, path_stat));
 		else
-			return handle_exec_error_else_part(data, exec_target);
+			return (handle_exec_error_else_part(data, exec_target));
 	}
 	return (0);
 }
 
-
-int handle_parent_process(pid_t pid)
+int	handle_parent_process(pid_t pid)
 {
-    int status;
-    int exit_code;
+	int	status;
+	int	exit_code;
 
-    waitpid(pid, &status, 0);
-    if (WIFEXITED(status))
-        exit_code = WEXITSTATUS(status);
-    else
-        exit_code = 128 + WTERMSIG(status);
-    
-    return exit_code;
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		exit_code = WEXITSTATUS(status);
+	else
+		exit_code = 128 + WTERMSIG(status);
+	return (exit_code);
 }
-int	execute_child_process(t_command *cmdtable, char **full_args, char **envp, t_data *data)
+
+int	execute_child_process(t_command *cmdtable, char **full_args, char **envp,
+		t_data *data)
 {
-	char *exec_target;
+	char	*exec_target;
 
 	exec_target = get_command_path(cmdtable->cmds);
 	if (!exec_target)
 		exec_target = cmdtable->cmds;
-
 	if (cmdtable->redirects != NULL)
 	{
 		if (setup_redirection(cmdtable->redirects) == -1)
@@ -44,11 +42,11 @@ int	execute_child_process(t_command *cmdtable, char **full_args, char **envp, t_
 	}
 	if (execve(exec_target, full_args, envp) == -1)
 		exit(handle_exec_error(data, exec_target));
-
 	return (0);
 }
 
-int	execute_external_command(t_command *cmdtable, char **full_args, char **envp, t_data *data)
+int	execute_external_command(t_command *cmdtable, char **full_args, char **envp,
+		t_data *data)
 {
 	pid_t	pid;
 
