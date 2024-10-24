4b5f0f31be8acf8f3d04562cda7da88628e0e69e
gru <damienantoine57@gmail.com>
2024-10-24 13:51:50 +0200

    helper_token_1.c norm

--- CommonCore/MINISHELL/srcs/m_lexer/helper_token_1.c
+++ CommonCore/MINISHELL/srcs/m_lexer/helper_token_1.c
@@ -1,12 +1,36 @@
 #include "../../headers/minishell.h"
 
-size_t	estimate_buffer_size(const char *str, t_data *data)
+size_t	ft_estimate_variable_size(const char *str, int *i, t_data *data)
 {
-	size_t	size;
-	int		i;
 	int		start;
 	char	*var_name;
 	char	*expanded_var;
+	size_t	size;
+
+	size = 0;
+	if (str[*i] == '?')
+	{
+		size += 11;
+		(*i)++;
+	}
+	else
+	{
+		start = *i;
+		while (str[*i] && (isalnum(str[*i]) || str[*i] == '_'))
+			(*i)++;
+		var_name = ft_substr(str, start, *i - start);
+		expanded_var = expand_variable(var_name, data);
+		size += ft_strlen(expanded_var);
+		free(var_name);
+		free(expanded_var);
+	}
+	return (size);
+}
+
+size_t	ft_estimate_buffer_size(const char *str, t_data *data)
+{
+	size_t	size;
+	int		i;
 
 	size = ft_strlen(str) + 1;
 	i = 0;
@@ -15,25 +39,20 @@ size_t	estimate_buffer_size(const char *str, t_data *data)
 		if (str[i] == '$')
 		{
 			i++;
-			if (str[i] == '?')
-			{
-				size += 11;
-				i++;
-			}
-			else
-			{
-				start = i;
-				while (str[i] && (isalnum(str[i]) || str[i] == '_'))
-					i++;
-				var_name = ft_substr(str, start, i - start);
-				expanded_var = expand_variable(var_name, data);
-				size += ft_strlen(expanded_var);
-				free(var_name);
-				free(expanded_var);
-			}
+			size += ft_estimate_variable_size(str, &i, data);
 		}
 		else
+		{
 			i++;
+		}
 	}
 	return (size);
+}
+
+size_t	estimate_buffer_size(const char *str, t_data *data)
+{
+	size_t	buffer_size;
+
+	buffer_size = ft_estimate_buffer_size(str, data);
+	return (buffer_size);
 }
