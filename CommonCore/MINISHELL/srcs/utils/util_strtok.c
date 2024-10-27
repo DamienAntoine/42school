53bb92c839fbfd51da8c3084336093b52b6c4770
sangha <putcharmaster@gmail.com>
2024-10-27 21:08:33 +0100

    util_strtok is done,  norminette is done

--- CommonCore/MINISHELL/srcs/utils/util_strtok.c
+++ CommonCore/MINISHELL/srcs/utils/util_strtok.c
@@ -1,5 +1,40 @@
 #include "../../headers/minishell.h"
 
+char	*initialize_str(char *str, const char *delimiter, char **last)
+{
+	if (str == NULL)
+		str = *last;
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	while (*str && ft_strchr(delimiter, *str))
+		str++;
+	if (*str == '\0')
+		return (NULL);
+	*last = str;
+	return (str);
+}
+
+char	*check_special_characters(char *str, char *start, char **last)
+{
+	if (str != start)
+	{
+		*last = str;
+		return (ft_substr(start, 0, str - start));
+	}
+	if (*(str + 1) == *str)
+		str++;
+	*last = str + 1;
+	return (ft_substr(start, 0, str - start + 1));
+}
+
+void	toggle_quotes(char c, int *in_single_quotes, int *in_double_quotes)
+{
+	if (c == '"' && !*in_single_quotes)
+		*in_double_quotes = !*in_double_quotes;
+	else if (c == '\'' && !*in_double_quotes)
+		*in_single_quotes = !*in_single_quotes;
+}
+
 char	*ft_strtok(char *str, const char *delimiter)
 {
 	static char	*last;
@@ -9,35 +44,17 @@ char	*ft_strtok(char *str, const char *delimiter)
 
 	in_single_quotes = 0;
 	in_double_quotes = 0;
-	if (str == NULL)
-		str = last;
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	while (*str && ft_strchr(delimiter, *str))
-		str++;
-	if (*str == '\0')
+	str = initialize_str(str, delimiter, &last);
+	if (!str)
 		return (NULL);
 	start = str;
 	while (*str)
 	{
-		if (*str == '"' && !in_single_quotes)
-			in_double_quotes = !in_double_quotes;
-		else if (*str == '\'' && !in_double_quotes)
-			in_single_quotes = !in_single_quotes;
-		else if (!in_single_quotes && !in_double_quotes)
+		toggle_quotes(*str, &in_single_quotes, &in_double_quotes);
+		if (!in_single_quotes && !in_double_quotes)
 		{
 			if (ft_strchr("<>|", *str))
-			{
-				if (str != start)
-				{
-					last = str;
-					return (ft_substr(start, 0, str - start));
-				}
-				if (*(str + 1) == *str)
-					str++;
-				last = str + 1;
-				return (ft_substr(start, 0, str - start + 1));
-			}
+				return (check_special_characters(str, start, &last));
 			if (ft_strchr(delimiter, *str))
 				break ;
 		}
